db290c191ee49f4385ffc0da234f2c6261ff4766
Ryan Lee <ryanlee7132@gmail.com>
2020-03-28 18:46:54 -0400

    Start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from './components/layout'
 import Main from './containers/main'
+import "./styles.css";
+import SideBar from "./components/sidebar";
 
 function App() {
   return (
     <Layout>
+	  <SideBar />
       <Main />
     </Layout>
   );
