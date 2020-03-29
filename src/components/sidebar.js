0e87c60d3b8ea90974f7134ccf193f2c73fb5310
Ryan Lee <ryanlee7132@gmail.com>
2020-03-28 20:07:08 -0400

    Update sidebar.js

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -4,21 +4,13 @@ import { stack as Menu } from "react-burger-menu";
 export default props => {
   return (
     <Menu>
-      <a className="menu-item" href="/">
-        1
-      </a>
+      <a className="menu-item" href="/">1</a>
 
-      <a className="menu-item" href="/">
-        2
-      </a>
+      <a className="menu-item" href="/">2</a>
 
-      <a className="menu-item" href="/">
-        3
-      </a>
+      <a className="menu-item" href="/">3</a>
 
-      <a className="menu-item" href="/">
-        4
-      </a>
+      <a className="menu-item" href="/">4</a>
     </Menu>
   );
 };
