9aaa48e94b61ab102dbff35ccf23fec756bb1d13
NhanLe1 <ntle1@pipeline.sbcc.edu>
2020-03-28 21:05:57 -0700

    update tags feature on register and login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from './components/layout'
 import Main from './containers/main'
+import Sidebar from './components/sidebar/sidebar';
 
 function App() {
   return (
     <Layout>
+      <Sidebar />
       <Main />
     </Layout>
   );
