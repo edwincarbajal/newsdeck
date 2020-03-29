6bcb145c9d4127c7a39a24cdc7970f8ad1340fc2
Ryan Lee <ryanlee7132@gmail.com>
2020-03-28 20:00:29 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Layout from './components/layout'
 import Main from './containers/main'
-import "./styles.css";
-import SideBar from "./components/sidebar";
 
 function App() {
   return (
