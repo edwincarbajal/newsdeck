e25b46273433a0a5486446d0a1367ce2d13ac383
Ryan Lee <ryanlee7132@gmail.com>
2020-03-29 13:01:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './components/layout'
 import Main from './containers/main'
 import Form from './login/form/Form';
-import LoginForm from './login/Login';
+import AuthenticationForm from './authentication/form/AuthenticationForm';
 
 class App extends Component {
 
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Layout />
+        <AuthenticationForm />
       </div>
     );
   }
