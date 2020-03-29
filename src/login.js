93fcaab68fc0164bf84dd317fdb1bcbc08245d3b
Ryan Lee <ryanlee7132@gmail.com>
2020-03-28 22:39:31 -0400

    Update login.js

--- src/login.js
+++ src/login.js
@@ -1,92 +1,101 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image, Text } from "react-native";
+import styled, { css } from "styled-components";
 import InputDefault from "../components/InputDefault";
 import ButtonDefaultStateFocus from "../components/ButtonDefaultStateFocus";
 import ButtonDefaultPrimary from "../components/ButtonDefaultPrimary";
 
 function SignUp(props) {
   return (
-    <View style={styles.root}>
-      <View style={styles.rectangle3}>
+    <Root>
+      <Rectangle3>
         <Image
-          source={require("../public/logo.png")}
-          resizeMode="contain"
-          style={styles.image}
+          src={require("../public/logo.png")}
         ></Image>
-        <Text style={styles.newsDeck}>NewsDeck</Text>
-        <InputDefault style={styles.inputDefault1}></InputDefault>
+        <NewsDeck>NewsDeck</NewsDeck>
+        <InputDefault
+          style={{
+            width: 277,
+            height: 47,
+            backgroundColor: "transparent",
+            opacity: 1,
+            marginTop: 178,
+            alignSelf: "center"
+          }}
+        ></InputDefault>
         <ButtonDefaultStateFocus
+          style={{
+            width: 277,
+            height: 52,
+            backgroundColor: "transparent",
+            opacity: 1,
+            marginTop: 11,
+            alignSelf: "center"
+          }}
           text1=""
-          style={styles.buttonDefaultStateFocus1}
         ></ButtonDefaultStateFocus>
         <ButtonDefaultPrimary
-          style={styles.buttonDefaultPrimary}
+          style={{
+            width: 179,
+            height: 47,
+            backgroundColor: "transparent",
+            opacity: 1,
+            marginTop: 17,
+            alignSelf: "center"
+          }}
         ></ButtonDefaultPrimary>
-        <Text style={styles.loremIpsum}>Don&#39;t have an account yet?</Text>
-      </View>
-    </View>
+        <LoremIpsum>Don&#39;t have an account yet?</LoremIpsum>
+      </Rectangle3>
+    </Root>
   );
 }
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    backgroundColor: "white"
-  },
-  rectangle3: {
-    width: 534,
-    height: 768,
-    backgroundColor: "rgba(255,255,255,1)",
-    marginTop: 12,
-    alignSelf: "center"
-  },
-  image: {
-    width: 113,
-    height: 114,
-    marginTop: 26,
-    marginLeft: 210
-  },
-  newsDeck: {
-    width: 146,
-    height: 54,
-    color: "#121212",
-    fontSize: 30,
-    fontFamily: "roboto-regular",
-    marginLeft: 194
-  },
-  inputDefault1: {
-    width: 277,
-    height: 47,
-    backgroundColor: "transparent",
-    opacity: 1,
-    marginTop: 178,
-    alignSelf: "center"
-  },
-  buttonDefaultStateFocus1: {
-    width: 277,
-    height: 52,
-    backgroundColor: "transparent",
-    opacity: 1,
-    marginTop: 11,
-    alignSelf: "center"
-  },
-  buttonDefaultPrimary: {
-    width: 179,
-    height: 47,
-    backgroundColor: "transparent",
-    opacity: 1,
-    marginTop: 17,
-    alignSelf: "center"
-  },
-  loremIpsum: {
-    width: 170,
-    height: 50,
-    color: "#121212",
-    fontSize: 14,
-    fontFamily: "roboto-regular",
-    marginTop: 37,
-    alignSelf: "center"
-  }
-});
+const Root = styled.div`
+  display: flex;
+  background-color: white;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Rectangle3 = styled.div`
+  width: 534px;
+  height: 768px;
+  background-color: rgba(255,255,255,1);
+  flex-direction: column;
+  display: flex;
+  margin-top: 12px;
+  align-self: center;
+`;
+
+const Image = styled.img`
+  width: 113px;
+  height: 100%;
+  margin-top: 26px;
+  margin-left: 210px;
+  object-fit: contain;
+`;
+
+const NewsDeck = styled.span`
+  font-family: System;
+  width: 146px;
+  height: 54px;
+  color: #121212;
+  font-size: 30px;
+  font-weight: regular;
+  font-style: normal;
+  margin-left: 194px;
+`;
+
+const LoremIpsum = styled.span`
+  font-family: System;
+  width: 170px;
+  height: 50px;
+  color: #121212;
+  font-size: 14px;
+  font-weight: regular;
+  font-style: normal;
+  margin-top: 37px;
+  align-self: center;
+`;
 
 export default SignUp;
