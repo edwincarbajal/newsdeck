f2ef3489182b8aebffe5de2d5192229282fea7ff
NhanLe1 <ntle1@pipeline.sbcc.edu>
2020-03-29 10:12:59 -0700

    add frontPage

--- src/authentication/recommendation/Recommendation.js
+++ src/authentication/recommendation/Recommendation.js
@@ -5,13 +5,10 @@ import './Recommendation.css'
 class Recommendation extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            items: ["food", "corona", "music", "new things", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "new things", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "new things", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "new things", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "new things", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "new things", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "new things", "food", "corona", "music", "Trump", "Crytocurrencies and stuff", "new things"]
-        }
     }
 
     renderTags = () => {
-        return this.state.items.map((tagName, i) => <Tag handleChoosingTopic={this.props.handleChoosingTopic} key={i} tagName={tagName}></Tag>);
+        return this.props.tags.map((tagName, i) => <Tag handleChoosingTopic={this.props.handleChoosingTopic} key={i} tagName={tagName}></Tag>);
     }
 
     render() {
